3e5057488471622de116fe23bdf01113f5433cb4
Shahzaib <shahzaibsiddiqui@live.com>
2025-02-11 00:55:01 +0500

    Updated main file

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello, Flask on gjhgjghjghjgjghj0!'
+jkhkjkhjk
+g
+hf
+h
+fgh
+fyhr677
 
 if __name__ == '__main__':
     app.run(debug=True)
